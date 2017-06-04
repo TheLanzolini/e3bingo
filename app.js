3e10043a20daaed7ab3f8b989d3720c680c9d446
Alex Lanzoni <alex@sportshubtech.com>
2017-06-04 05:09:28 -0400

    bingo board done

---
+++ app.js
@@ -0,0 +1,116 @@
+var congfig;
+var cards = {
+  0: '"is truly open world"',
+  1: 'Spider Man!',
+  2: 'Ubisoft opens with Just Dance',
+  3: 'SPORTS!',
+  4: 'Master Chief is shown',
+  5: 'Stage Fright',
+  6: 'An Early Access Game on stage',
+  7: 'Atlus JRPG gameplay shown on switch',
+  8: 'Japanese Dude w/ Translator',
+  9: 'Sony reveals new hardware (not a color)',
+  10: 'Call of Duty Modern Warfare 2 Remastered',
+  11: 'Minecraft: Scorpio Edition',
+  12: 'Someone snaps their fingers (switch noise)',
+  13: 'VR Shows up',
+  14: 'Splatoon eSports',
+  15: 'Mario and Sonic in the same game',
+  16: 'star wars games show gameplay',
+  17: 'Famous person on stage that doesn\'t want to be there',
+  18: 'Mixer promotion - live on twitch',
+  19: 'David Cage',
+  20: 'KOJIMA',
+  21: 'Valve shows up',
+  22: '"Hip" Exec w/ jeans + game shirt + blazer',
+  23: '"It\'s all about you, the fans"',
+  24: 'Bethesda launches another mobile game',
+  25: 'Tom Fucking Clancy',
+  26: 'Metroid bitches!',
+  27: 'On stage music that shouldn\'t be there',
+  28: 'Ambiguous Exclusively from Microsoft',
+  29: 'KH3',
+  30: 'Hitman',
+  31: 'Cuphead',
+  32: 'Someone makes a nerd joke',
+  33: 'Remastered Game',
+  34: 'Movie Trailer',
+  35: 'PRE ORDER NOW!',
+  36: 'New First Party Nintendo title',
+  37: 'SMAYSH BRUDDAS',
+  38: 'KNACK 2 BAYBEEE',
+  39: 'Indie Game Shown',
+  40: 'Brand New IP',
+  41: 'Mobile Game Appears',
+  42: 'Awkward, scripted "team-speak" multiplayer demo'
+}
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array;
+}
+
+var keys = Object.keys(cards);
+
+var shuffled = shuffleArray(keys);
+
+shuffled = shuffled.slice(0, 25);
+
+function init(){
+  var APP = document.getElementById('app');
+  while(APP.lastChild){
+    APP.removeChild(APP.lastChild);
+  }
+  if(localStorage.getItem('config')){
+    config = localStorage.getItem('config');
+  }
+  var header = document.createElement('div');
+  header.classList.add('header');
+  var title = document.createElement('h1');
+  title.innerHTML = 'E3 BINGO!';
+  header.appendChild(title);
+  var about = document.createElement('div');
+  about.classList.add('about');
+  about.innerHTML = 'About';
+  about.addEventListener('click', function(){
+    location.replace('/about');
+  });
+  header.appendChild(about);
+
+  APP.appendChild(header);
+
+  var table = document.createElement('div');
+  table.classList.add('table');
+  var rowElem;
+  shuffled.forEach(function(key, index){
+    var cardString = cards[key];
+    if(index >= 25){
+      return;
+    }
+    if(index % 5 == 0 ){
+      rowElem = document.createElement('div');
+      rowElem.classList.add('row');
+      rowElem.setAttribute('row', index/5);
+      table.appendChild(rowElem);
+    }
+    var cellElem = document.createElement('div');
+    cellElem.classList.add('cell');
+    cellElem.setAttribute('cell', index);
+    cellElem.setAttribute('type', key);
+    cellElem.style.backgroundImage = "url('images/"+ key +".jpg')"
+    if(index == 12){
+      cellElem.innerHTML = 'FREE'
+    }else{
+      cellElem.innerHTML = cardString;
+    }
+    rowElem.appendChild(cellElem);
+  });
+  APP.appendChild(table);
+
+
+}
+document.addEventListener('DOMContentLoaded', init);
